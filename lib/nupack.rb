aff4c347e7f63866d07540bc73d1965ff39c1ca5
Matthew de Verteuil <mverteuil@Toronto.local>
2016-09-27 19:34:18 -0400

    Changed interface

--- lib/nupack.rb
+++ lib/nupack.rb
@@ -1,6 +1,16 @@
 # vim: sw=2 ts=2 et
 class ProjectPricer
-  def calculate(base_price, num_workers, material)
+  attr_reader :base_price
+  attr_reader :num_workers
+  attr_reader :material 
+
+  def initialize(base_price, num_workers, material)
+    @base_price = base_price
+    @num_workers = num_workers
+    @material = material
+  end
+
+  def calculate
     1_591.58
   end
 end
--- spec/nupack_spec.rb
+++ spec/nupack_spec.rb
@@ -3,10 +3,17 @@ require 'nupack'
 
 describe ProjectPricer do
   describe '#calculate' do
-    pricer = described_class.new
     context 'with $1,299.99, 3 people and food' do
+      pricer = described_class.new 1_299.99, 3, 'food'
       it 'returns $1,591.58' do
-        expect(pricer.calculate(1_299.99, 3, 'food')).to eq(1_591.58) 
+        expect(pricer.calculate).to eq(1_591.58) 
+      end
+    end
+
+    context 'with $5,432.00, 1 person and drugs' do
+      pricer = described_class.new 5_432.00, 1, 'drugs'
+      it 'returns $6,199.81' do
+        expect(pricer.calculate).to eq(6_199.81)
       end
     end
   end
