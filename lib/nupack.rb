31e3388f272f167ee1d001cdd496b07705b0b127
M. de Verteuil <onceuponajooks@gmail.com>
2016-09-28 18:38:58 -0400

    Update nupack.rb
    Fixed comment

--- lib/nupack.rb
+++ lib/nupack.rb
@@ -22,7 +22,7 @@ class ProjectPricer
     tier_1_markup_values = tier_1_markups.map { |markup| markup.check(self) ? markup.apply(self) : 0 }
     @base_price += tier_1_markup_values.reduce(:+)
 
-    # Tier 2 markups are applied after Tier 2 markups have been included in the base price
+    # Tier 2 markups are applied after Tier 1 markups have been included in the base price
     tier_2_markups = [WorkerMarkup, MaterialMarkup]
     tier_2_markup_values = tier_2_markups.map { |markup| markup.check(self) ? markup.apply(self) : 0 }
     final_price = @base_price + tier_2_markup_values.reduce(:+)
