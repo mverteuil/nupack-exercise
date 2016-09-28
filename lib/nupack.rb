4a475850fd885d4e8fb07f1ebf868e9a66235aa5
Matthew de Verteuil <mverteuil@Toronto.local>
2016-09-28 18:29:40 -0400

    Implementation passed two examples

--- lib/nupack.rb
+++ lib/nupack.rb
@@ -17,7 +17,16 @@ class ProjectPricer
   end
 
   def calculate
-    @material != 'drugs' ? 1_591.58 : 6_199.81
+    # Tier 1 markups are applied to the base price
+    tier_1_markups = [FlatMarkup]
+    tier_1_markup_values = tier_1_markups.map { |markup| markup.check(self) ? markup.apply(self) : 0 }
+    @base_price += tier_1_markup_values.reduce(:+)
+
+    # Tier 2 markups are applied after Tier 2 markups have been included in the base price
+    tier_2_markups = [WorkerMarkup, MaterialMarkup]
+    tier_2_markup_values = tier_2_markups.map { |markup| markup.check(self) ? markup.apply(self) : 0 }
+    final_price = @base_price + tier_2_markup_values.reduce(:+)
+    (final_price * 100.0).round / 100.0
   end
 end
 
